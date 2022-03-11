034c980908cd23394eef071269cf6ac8e61a9eee
Alan Sartorio <alan42ga@hotmail.com>
2022-03-11 09:07:54 -0300

    Started implementing in sticker colors.

--- TP1/cube.py
+++ TP1/cube.py
@@ -1,5 +1,51 @@
 import itertools
 import functools
+from enum import Enum, auto
+from typing import DefaultDict
+
+
+class Colors(Enum):
+    RED = auto()
+    ORANGE = auto()
+    WHITE = auto()
+    YELLOW = auto()
+    BLUE = auto()
+    GREEN = auto()
+
+    def to_string(self):
+        return '\033[1;' + {
+            Colors.RED: '31m',
+            Colors.ORANGE: '36m',
+            Colors.WHITE: '37m',
+            Colors.YELLOW: '33m',
+            Colors.BLUE: '34m',
+            Colors.GREEN: '32m',
+        }[self] + '█\033[0m'
+        # return {
+            # Colors.RED: 'r',
+            # Colors.ORANGE: 'o',
+            # Colors.WHITE: 'w',
+            # Colors.YELLOW: 'y',
+            # Colors.BLUE: 'b',
+            # Colors.GREEN: 'g',
+        # }[self]
+
+
+piece_colors = (
+    (Colors.RED, Colors.WHITE, Colors.BLUE),
+    (Colors.GREEN, Colors.WHITE, Colors.RED),
+    (Colors.BLUE, Colors.YELLOW, Colors.RED),
+    (Colors.RED, Colors.YELLOW, Colors.GREEN),
+
+    (Colors.BLUE, Colors.WHITE, Colors.ORANGE),
+    (Colors.ORANGE, Colors.WHITE, Colors.GREEN),
+    (Colors.ORANGE, Colors.YELLOW, Colors.BLUE),
+    (Colors.GREEN, Colors.YELLOW, Colors.ORANGE),
+)
+
+
+def rotate_3(a, b, c):
+    return c, a, b
 
 
 class Piece:
@@ -13,6 +59,12 @@ class Piece:
 
     def __repr__(self) -> str:
         return f'{self.id}{self.orientation}'
+
+    def get_colors(self) -> tuple[Colors, Colors, Colors]:
+        colors = piece_colors[self.id]
+        # for _ in range(self.orientation):
+            # colors = rotate_3(*colors)
+        return colors
 
 
 class Cube:
@@ -37,6 +89,53 @@ class Cube:
     def clone(self) -> 'Cube':
         return Cube.parse(repr(self))
 
+    def colored_faces(self):
+        pc = []
+        for i, piece in enumerate(self.pieces):
+            a, b, c = piece.get_colors()
+            if i in (1, 2, 4, 7):
+                a, b, c = c, b, a
+            a, b, c = rotate_3(a, b, c)
+            pc.append((a, b, c))
+
+        up = ((pc[3][0], pc[1][0]), (pc[2][0], pc[0][0]))
+        down = ((pc[6][0], pc[4][0]), (pc[7][0], pc[5][0]))
+        right = ((pc[0][1], pc[1][1]), (pc[4][1], pc[5][1]))
+        left = ((pc[3][1], pc[2][1]), (pc[7][1], pc[6][1]))
+        front = ((pc[2][2], pc[0][2]), (pc[6][2], pc[4][2]))
+        back = ((pc[1][2], pc[3][2]), (pc[5][2], pc[7][2]))
+
+        return (up, down, right, left, front, back)
+
+    def to_string(self):
+        (up, down, right, left, front, back) = self.colored_faces()
+        faces = {
+            'u': up,
+            'd': down,
+            'r': right,
+            'l': left,
+            'f': front,
+            'b': back,
+        }
+
+        text = ["  uu    ",
+                "  uu    ",
+                "llffrrbb",
+                "llffrrbb",
+                "  dd    ",
+                "  dd    "]
+        text = [[c for c in row] for row in text]
+
+        counters = {k: 0 for k in "udrlfb"}
+        for y, row in enumerate(text):
+            for x, c in enumerate(row):
+                if c in counters:
+                    count = counters[c]
+                    text[y][x] = faces[c][count // 2][count % 2].to_string()
+                    counters[c] += 1
+
+        return "\n".join(["".join(row) for row in text])
+
 
 class Turn:
     def __init__(self, cycle: tuple[int, int, int, int], orientation: int):
@@ -51,10 +150,10 @@ class Turn:
         cube = cube.clone()
         c0, c1, c2, c3 = self.cycle
         tmp = cube.pieces[c0]
-        cube.pieces[c0] = cube.pieces[c1]
-        cube.pieces[c1] = cube.pieces[c2]
-        cube.pieces[c2] = cube.pieces[c3]
-        cube.pieces[c3] = tmp
+        cube.pieces[c0] = cube.pieces[c3]
+        cube.pieces[c3] = cube.pieces[c2]
+        cube.pieces[c2] = cube.pieces[c1]
+        cube.pieces[c1] = tmp
         for i in self.cycle:
             o = cube.pieces[i].orientation
 
@@ -62,7 +161,6 @@ class Turn:
                 o = (o + 1) % 3
                 if o == self.orientation:
                     o = (o + 1) % 3
-            
             cube.pieces[i].orientation = o
         return cube
 
@@ -72,4 +170,6 @@ turns = {
     'L': Turn((6, 7, 3, 2), 1),
 }
 
+print(Cube.solved()[0].to_string())
+print(turns['R'].next(Cube.solved()[0]).to_string())
 print(turns['R'].next(Cube.solved()[0]))
