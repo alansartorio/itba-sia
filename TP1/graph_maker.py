c5857ae942896473bfc77bbd1178d19dd495d39a
Alan Sartorio <alan42ga@hotmail.com>
2022-03-15 06:28:03 -0300

    Added comparison for heuristic functions to graphs.

--- TP1/graph_maker.py
+++ TP1/graph_maker.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from tqdm.std import tqdm
 from cube import Cube, generate_scrambled, solved_cubes
 from tree import Tree, Node, HeuristicTree, HeuristicNode
-from heuristics import move_count_combination, sticker_groups
+from heuristics import move_count_combination, sticker_groups, manhattan_distance
 import time
 from methods import *
 from multiprocessing import Process
@@ -19,18 +19,36 @@ def test_global_heuristics_0(cube: Cube):
     return test_global_heuristics(cube, move_count_combination)
 def test_global_heuristics_cost_0(cube: Cube):
     return test_global_heuristics_cost(cube, move_count_combination)
+def test_local_heuristics_0(cube: Cube):
+    return test_local_heuristics(cube, move_count_combination)
+def test_global_heuristics_0(cube: Cube):
+    return test_global_heuristics(cube, move_count_combination)
+def test_global_heuristics_cost_0(cube: Cube):
+    return test_global_heuristics_cost(cube, move_count_combination)
+def test_local_heuristics_0(cube: Cube):
+    return test_local_heuristics(cube, move_count_combination)
+def test_global_heuristics_0(cube: Cube):
+    return test_global_heuristics(cube, move_count_combination)
+def test_global_heuristics_cost_0(cube: Cube):
+    return test_global_heuristics_cost(cube, move_count_combination)
 
 methods = {
-        "bpp": test_bpp,
-        "bppv": test_bppv,
-        "bpa": test_bpa,
-        "local_heuristic": test_local_heuristics_0,
-        "global_heuristic": test_global_heuristics_0,
-        "global_heuristic_cost": test_global_heuristics_cost_0
+        # "bpp": test_bpp,
+        # "bppv": test_bppv,
+        # "bpa": test_bpa,
+        "local_heuristic (Move Count Combination)": lambda cube: test_local_heuristics(cube, move_count_combination),
+        "global_heuristic (Move Count Combination)": lambda cube: test_global_heuristics(cube, move_count_combination),
+        "global_heuristic_cost (Move Count Combination)": lambda cube: test_global_heuristics_cost(cube, move_count_combination),
+        "local_heuristic (Sticker Groups)": lambda cube: test_local_heuristics(cube, sticker_groups),
+        "global_heuristic (Sticker Groups)": lambda cube: test_global_heuristics(cube, sticker_groups),
+        "global_heuristic_cost (Sticker Groups)": lambda cube: test_global_heuristics_cost(cube, sticker_groups),
+        "local_heuristic (Manhattan Distance)": lambda cube: test_local_heuristics(cube, manhattan_distance),
+        "global_heuristic (Manhattan Distance)": lambda cube: test_global_heuristics(cube, manhattan_distance),
+        "global_heuristic_cost (Manhattan Distance)": lambda cube: test_global_heuristics_cost(cube, manhattan_distance),
 }
 
 def thread_time():
-    return time.clock_gettime(time.CLOCK_THREAD_CPUTIME_ID)
+    return time.clock_gettime(time.CLOCK_REALTIME)
 
 def timeout(func, timeout: float):
     p = Process(target=func)
@@ -40,7 +58,7 @@ def timeout(func, timeout: float):
 
 def time_method(method):
     start = thread_time()
-    output = timeout(method, 2)
+    output = timeout(method, 1)
     end = thread_time()
     return end - start, output
 
@@ -58,7 +76,7 @@ def test_scramble(cube: Cube):
 
 def test_sampled(scramble_count: int):
     method_sums = defaultdict(lambda:0)
-    sample_count = 20
+    sample_count = 5
     for sample in tqdm(range(sample_count), leave=False):
         # print(f'Sample: {sample}')
         times = test_scramble(generate_scrambled(scramble_count))
@@ -74,7 +92,7 @@ def test_sampled(scramble_count: int):
 
 
 times_by_method = defaultdict(lambda:[])
-counts = list(range(0, 10))
+counts = list(range(0, 20))
 for count in tqdm(counts, leave=False):
     # print(f'Scramble depth: {count}')
     results = test_sampled(count)
