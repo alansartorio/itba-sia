bc2f90ac53ae177e5cca313c5f7224f70d7aebbc
Alan Sartorio <alan42ga@hotmail.com>
2022-04-04 22:19:41 -0300

    Updated plot.

--- TP2/plot.py
+++ TP2/plot.py
@@ -22,31 +22,40 @@ with open('plot_data.json', 'r') as file:
 def convert_data():
     max_generations = max(len(d['result']['fitnesses']) for d in data)
     for d in data:
-        yield (d['algorythm']['population_count'], d['algorythm']['mutation']['probability'], d['algorythm']['crossover']['type'], d['algorythm']['selection']['type']), take(max_generations, padnone(d['result']['fitnesses']))
+        for i, v in enumerate(take(max_generations, padnone(d['result']['fitnesses']))):
+            yield i, d['algorythm']['population_count'], d['algorythm']['mutation']['probability'], d['algorythm']['crossover']['type'], d['algorythm']['selection']['type'], v
 
 
-key_columns = ('Population Size', 'Mutation Probability',
+key_columns = ('Generation', 'Population Size', 'Mutation Probability',
                'Crossover Type', 'Selection Type')
 value_columns = ('Fitness', )
-df = pd.DataFrame(dict(convert_data()))
-df.columns.names = key_columns
-# df['Keys'] = df[df.columns[:-1]].apply(
-# lambda x: ','.join(x.dropna().astype(str)),
-# axis=1
-# )
-# df = df[['Keys', 'Fitness']]
-# print(df)
-# print(type(df.index))
-# df = df.melt(value_vars=df.columns)
-# print(df.columns.names)
-df.columns = pd.MultiIndex.from_tuples(df.columns, names=key_columns)
-# df.index = pd.MultiIndex.from_frame(df)
-# df = df.melt(id_vars=[(0, 1, 2, 3)])
-# print(df)
-df.plot()
-# df.rename(columns=lambda tup:','.join(map(str, tup)), inplace=True)
-# print(df.columns.groupby(lambda v:v[0] == 10))
-# df.columns = df.columns.map(partial(map, str)).map('|'.join).str.strip('|')
-# print(df.columns)
-# sns.lineplot(data=df.columns)
+df = pd.DataFrame(convert_data(), columns=key_columns + value_columns)
+# df.index
+df.set_index(list(key_columns[1:] + ('Generation', )), inplace=True)
+# df.set_index('Generation', inplace=True)
+# df = df.explode('Fitness')
+# df = df.melt(id_vars='index', value_vars=value_columns)
+# df.plot()
+# sns.pointplot(data=df)
+# df.plot.(y='Fitness')#, x=['Generation', 'Population Size'])
+# df['Fitness'].plot()
+# df = df.drop('Crossover Type', axis=1)
+# df = df.drop('Selection Type', axis=1)
+groups = ['Population Size', 'Mutation Probability', 'Crossover Type', 'Selection Type']
+df.groupby(level=groups)['Fitness'].plot(legend=True, use_index=False)
+plt.legend(title=', '.join(groups), loc='center left', bbox_to_anchor=(1, 0.5))
+plt.subplots_adjust(left=0.1, right=0.7, top=0.9, bottom=0.1)
+plt.tight_layout()
+# df.groupby(groups)['Fitness'].plot.line(ylabel='Fitness', legend=True)
+
+# sns.pointplot(df.groupby(list(key_columns[1:]))['Fitness'])
 plt.show()
+# # df = df.pivot(columns=value_columns)
+# # df = df.set_index(list(key_columns)).mean()
+# # df.set_index()
+# # df.columns.names = key_columns
+
+# # df.columns = pd.MultiIndex.from_tuples(df.columns, names=key_columns)
+# # sns.pointplot(data=df)
+# # df.plot()
+# plt.show()
