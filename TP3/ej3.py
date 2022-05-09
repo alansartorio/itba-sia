Fixed bug in Network backpropagation and added visualizations.

TP3/ej3.py
@@ -1,3 +1,4 @@
+from image_plot import ImageEvaluationPlot
 from activation_functions import get_sigmoid_tanh, lineal_func, step_func
 from network import Network
 from train_data import ej1_and_data, ej1_xor_data
@@ -6,55 +7,32 @@ import numpy as np
 from plot_line import Plot
 
 
-model = Network.with_random_weights(2, (3, 1, ), step_func)
+# model = Network.with_random_weights(2, (3, 1, ), step_func)
 # model = Network.with_random_weights(2, (4, 4, 4, 4, 1, ), step_func)
 # model = Network.with_random_weights(2, (3, 3, 1, ), lineal_func)
-# model = Network.with_random_weights(2, (3, 1, ), *get_sigmoid_tanh(1))
+model = Network.with_random_weights(2, (100, 100, 100, 1, ), *get_sigmoid_tanh(100))
 
 # data = ej1_and_data
 data = ej1_xor_data
 
-res = 0.025
-coordinates = np.mgrid[-2:2:res, 2:-2:-res]
-# print(coordinates.shape)
-# coordinates = np.array(np.meshgrid([1, -1], [-1, 1]))
-
-minX = np.min(coordinates[0])
-maxX = np.max(coordinates[0])
-minY = np.min(coordinates[1])
-maxY = np.max(coordinates[1])
-
-fig, ax = plt.subplots()
-
-# plt.ion()
-image = ax.imshow(np.zeros(coordinates.shape[1:]), extent=(minX, maxX, minY, maxY), vmin=-1, vmax=1)#, cmap='Wistia')
-ax.scatter([-1, -1, 1, 1], [-1, 1, -1, 1], color='black')
-def draw():
-    out = model.evaluate(coordinates)
-    out = np.swapaxes(out, 0, -1)
-    # out = np.ones(coordinates.shape[1:])
-    image.set_array(out)
-
-    
-    # plt.draw()
-    fig.canvas.draw()
-    fig.canvas.start_event_loop(0.0000001)
-
-draw()
-plt.show(block=False)
+res = 120
+plot = ImageEvaluationPlot(model, res, res, (-2, 2), (-2, 2))
+plot.ax.scatter([-1, -1, 1, 1], [-1, 1, -1, 1], color='black')
+plot.draw()
 
 
 # print(model.layers[0].weights.flatten())
 # print(model.error(ej1_data))
 while model.error(data) > 0:
-    model.train(0.1, data)
-    print(model.layers[0].weights.flatten(), model.error(data))
+    # model.train(0.001, data)
+    model.train(0.000001, data)
+    print(model.error(data))
     # print(model.layers[0].weights.flatten(), model.error(ej1_data))
     # for single_data in data:
         # print(single_data.inputs, single_data.outputs, model.evaluate(single_data.inputs), end=' | ')
     # print()
 
-    draw()
+    plot.draw()
 plt.show()
 
 # single_neuron = Network.with_random_weights(1, (2, 3), step_func)

TP3/network.py
@@ -69,7 +69,7 @@ class Network(ABC):
         return self.calculate_vs_and_hs(inputs)[-1].v
 
     def error(self, evaluation_data: Sequence[SingleData]):
-        return 0.5 * sum((data.outputs - self.evaluate(data.inputs)) ** 2 for data in evaluation_data)
+        return 0.5 * sum(np.sum((data.outputs - self.evaluate(data.inputs)) ** 2) for data in evaluation_data)
 
     def calculate_deltas(self, single_data: SingleData, evaluation_data: list[EvaluationData]):
         last_hs = evaluation_data[-1].h
@@ -79,8 +79,8 @@ class Network(ABC):
             last_hs) * (single_data.outputs - last_vs)
         deltas = [last_layer_delta]
 
-        for layer in reversed(self.layers[1:]):
-            hs = evaluation_data[-1].h
+        for layer, layer_evalutation in reversed(list(zip(self.layers[1:], evaluation_data[1:]))):
+            hs = layer_evalutation.h
             deltas.insert(0, layer.calculate_previous_delta(hs, deltas[0]))
 
         return deltas
@@ -101,6 +101,10 @@ class Network(ABC):
             # self.train_single(learning_rate, single_data)
         self.train_single(learning_rate, random.sample(train_data, 1)[0])
 
+    def randomize_weights(self, amplitude: float = 0.01):
+        for layer in self.layers:
+            layer.weights[:,:] = (np.random.rand(*layer.weights.shape) * 2 - 1) * amplitude
+
     @classmethod
     def with_zeroed_weights(cls, input_size: int, layers: tuple[int, ...], activation_function: Callable[[np.ndarray], np.ndarray], derivated_activation_function: Callable[[np.ndarray], np.ndarray] = lambda x: np.full(x.shape, 1)):
         # + 1 to add threshold value
@@ -109,5 +113,7 @@ class Network(ABC):
 
     @classmethod
     def with_random_weights(cls, input_size: int, layers: tuple[int, ...], activation_function: Callable[[np.ndarray], np.ndarray], derivated_activation_function: Callable[[np.ndarray], np.ndarray] = lambda x: np.full(x.shape, 1)):
-        return cls(tuple((np.random.rand(current, previous + 1) * 2 - 1) * 0.1 for previous, current in zip((input_size, ) + layers, layers)), activation_function, derivated_activation_function)
+        m = cls.with_zeroed_weights(input_size, layers, activation_function, derivated_activation_function)
+        m.randomize_weights()
+        return m
 