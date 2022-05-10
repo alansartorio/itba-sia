1d1624f79819c8972026c0a0e59d8574345fd9d8
lucas <lugomez@itba.edu.ar>
2022-05-10 02:42:18 -0300

    Added random inputs in digits

--- TP3/digits.py
+++ TP3/digits.py
@@ -1,5 +1,6 @@
 import numpy as np
 from single_data import SingleData
+from typing import Sequence
 
 
 def read_digits():
@@ -31,3 +32,46 @@ def get_digits_data():
         # digits_data.append(SingleData(digit.flatten().astype(np.float64), out))
         
     return digits_data
+
+
+def rand_bit(value): 
+    if np.random.random() < 0.002:
+                if value  == 1:
+                    return -1
+                else:
+                    return 1
+    return value
+            
+def random_digits_data():
+    digits_data = []
+    empty_out = np.full((10,), -1, dtype=np.float64)
+    for i, digit in enumerate(read_digits()):
+        out = empty_out.copy()
+        out[i] = 1
+        #digits_data.append(SingleData(digit.flatten().astype(np.float64) * 2 - 1, out))
+        digits_data.append(SingleData(list(map(rand_bit, digit.flatten().astype(np.float64) * 2 - 1)), out))
+        # digits_data.append(SingleData(digit.flatten().astype(np.float64), out))
+        print(digit.flatten().astype(np.float64) * 2 - 1)
+    return digits_data
+
+"""
+def random_digits_data(prob: float):
+    digits_data = get_digits_data()
+    inputs = [d.inputs for d in digits_data]
+
+    new_inputs = list()
+    for digit in inputs:
+        d = []
+        for bit in digit:
+            value = np.random.random()
+            if  value < 0.02:
+                if bit == 1:
+                    bit = -1
+                else: 
+                    bit = 1
+            d.append(bit)
+        new_inputs.append(d)
+    print(inputs)
+    print(new_inputs)
+    return digits_data
+"""
--- TP3/ej3_3.py
+++ TP3/ej3_3.py
@@ -1,5 +1,5 @@
 from typing import Generator, TypeVar
-from digits import get_digits_data
+from digits import get_digits_data, random_digits_data
 from simple_image_plot import ImagePlot
 from single_data import SingleData
 from activation_functions import get_sigmoid_exp, get_sigmoid_tanh, softmax
@@ -8,6 +8,7 @@ import numpy as np
 from more_itertools import take
 
 digits_data = get_digits_data()
+random_digits_data = random_digits_data()
 
 model = Network.with_zeroed_weights(5 * 7, (10, 10,), *get_sigmoid_tanh(10))
 model.randomize_weights(0.01)
@@ -21,10 +22,10 @@ plot.ax.set_ylabel("Prediction")
 while error > 0:
     model.train(0.001, digits_data)
     error = model.error(digits_data)
-    evaluation = model.evaluate(np.array([digit.inputs for digit in digits_data]))
+    evaluation = model.evaluate(np.array([digit.inputs for digit in random_digits_data]))
     evaluation = softmax(evaluation)
     plot.draw(evaluation)
-    print(error)
+   # print(error)
 
 import matplotlib.pyplot as plt
 plt.show()
