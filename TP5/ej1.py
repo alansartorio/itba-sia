3d7b018909b255ad15f1fc8a35d9a0e2889c200f
Bruno Volcovinsky <brunovolko@gmail.com>
2022-06-16 12:38:13 -0300

    Fonts functions

---
+++ TP5/ej1.py
@@ -0,0 +1,9 @@
+import numpy as np
+import fonts
+
+#En fonts.font_1 hay simbolos
+#En fonts.font_2 hay mayuscula
+#En fonts.font_3 hay minuscula
+#Con fonts.to_bin_array(simbolo) se obtiene array de bits
+#En fonts.print_char(bin_array) se imprime una letra
+
